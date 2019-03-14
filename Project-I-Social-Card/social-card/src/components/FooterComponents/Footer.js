4c0d3218cdea18e22017602879e981d186695f50
William Soukkachang <WSoukkachang@yahoo.com>
2019-03-13 22:44:44 -0500

    removed borders

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -6,11 +6,17 @@ import loveicon from "./img/Love-Icon.png"
 import messageicon from "./img/Message-Icon.png"
 import shareicon from "./img/Share-Icon.png"
 
+// button.onclick = function() {
+//     count += 1;
+//     button.innerHTML = "" + count;
+//   };
+
 
 const Footer= () => {
     return (
       <section className="Footer">
       <img src={commenticon} className="CommentIcon"/>
+
       <img src={loveicon} className="LoveIcon"/>
       <img src={messageicon} className="MessageIcon"/>
       <img src={shareicon} className="ShareIcon"/>
