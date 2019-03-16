d3db5a2f0b52451ada40c2c267fa53bee0d21dd2
William Soukkachang <WSoukkachang@yahoo.com>
2019-03-16 00:04:24 -0500

    finished display, completed mvp

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,41 +1,51 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import NumberButton from './components/ButtonComponents/NumberButton';
-import ButtonContainer from './components/ButtonComponents/ButtonContainer';
 import ActionButton from './components/ButtonComponents/ActionButton';
 
 const numbers = [
-  {text: 1},
-  {text: 2},
-  {text: 3},
+  {text: 7, style: {backgroundColor: "blue", color: "red"}},
+  {text: 8},
+  {text: 9},
   {text: 4},
   {text: 5},
   {text: 6},
-  {text: 7},
-  {text: 7},
-  {text: 9}
+  {text: 1},
+  {text: 2},
+  {text: 3}
 ]
 
-const math = [
+const symbols = [
   {text: "÷"},
   {text: "x"},
   {text: "-"},
+  {text: "+"},
   {text: "="}
 ]
 
 const App = () => {
   return (
-    <div>
-      {numbers.map((button) => (
-      <NumberButton text={numbers.text}/>
-      ))
-      }
-      
-      {math.map((button) => (
-      <ActionButton text={math.text}/>
+    <div className="container">
+    <div className="calc-display"><CalculatorDisplay display={0} /></div>
+    <div className="clear-container"><ActionButton text={"clear"} buttonStyle={'clear-button'}/></div>
+    
+    <div className="symbols-container">
+      {symbols.map((symbol) => (
+      <NumberButton key={symbol.text} text={symbol.text} buttonStyle={'symbol-button'}/>
       ))
       }
     </div>
+
+    <div className="numbers-container">
+      {numbers.map((numbers) => (
+      <NumberButton text={numbers.text}/>
+      ))
+      }
+    </div>
+    
+    <div className="zero-container"><ActionButton text={0}/></div>
+     </div>
   );
 };
 
